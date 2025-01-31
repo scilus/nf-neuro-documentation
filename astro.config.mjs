f6860855a7078d08fbeb1ea31f00618a8dd51834
AlexVCaron <alex.valcourt.caron@usherbrooke.ca>
2025-01-31 22:36:09 +0000

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://alexvcaron.github.io",
+  site: "https://alexvcaron.github.io/dummy-boy",
   base: "/dummy-boy",
   markdown: {
     smartypants: true,
