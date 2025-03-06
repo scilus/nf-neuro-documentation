1015266cad538b729c0206958dd2ceeaf47ceb55
Anthony Gagnon <anthony.gagnon7@usherbrooke.ca>
2025-03-06 14:53:31 -0500

    switch to starlight as backend + major reorganization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,69 +1,75 @@
-import { defineConfig } from "astro/config";
-import rehypeExternalLinks from "rehype-external-links";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import mdx from "@astrojs/mdx";
-import icon from "astro-icon"
-
-
-const rehypePlugins = [
-  [  rehypeExternalLinks,
-    {
-      target: "_blank",
-    },
-  ]
-];
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://scilus.github.io",
-  base: "/nf-neuro",
-  markdown: {
-    smartypants: true,
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      // theme: "catppuccin-mocha",
-      themes: {
-        light: "catppuccin-latte",
-        dark: "catppuccin-macchiato",
-      },
-    },
-    rehypePlugins: rehypePlugins,
-    prefetch: true,
-  },
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    mdx({
-      rehypePlugins: rehypePlugins,
-      gfm: true,
-    }),
-    icon({
-      svgoOptions: {
-        multipass: true,
-        plugins: [
-          {
-            name: "preset-default",
-            params: {
-              overrides: {
-                // customize default plugin options
-                inlineStyles: {
-                  onlyMatchedOnce: false,
-                },
-                // or disable plugins
-                removeDoctype: false,
-              }
-            }
-          }
-        ]
-      }
-    })
-  ],
-  content: {
-    collections: {
-      docs: {} // Define docs collection
-    }
-  }
+	site: "https://scilus.github.io",
+	base: "/nf-neuro",
+	integrations: [
+		starlight({
+			title: 'nf-neuro',
+			logo: {
+				light: './src/assets/nf-neuro_logo_light.svg',
+				dark: './src/assets/nf-neuro_logo_dark.svg',
+				replacesTitle: true,
+			},
+			customCss: [
+				'./src/styles/custom.css',
+			],
+			social: {
+				github: 'https://github.com/scilus/nf-neuro.git',
+			},
+			sidebar: [
+				{
+					label: 'Custom pipeline',
+					items: [
+						{ label: 'Example Pipeline', slug: 'custom-pipeline/prototyping' },
+					],
+				},
+				{	
+					label: 'How-to',
+					items: [
+						{ label: 'Top-5 Operators', slug: 'how-to/top-5-operators' },
+						{ label: 'Custom Subworkflows', slug: 'how-to/custom-subworkflows' },
+					],
+				},
+				{
+					label: 'Advanced Tutorials',
+					items: [
+						{ label: 'BIDS Input', slug: 'advanced-tutorials/bidsinput' },
+						{ label: 'BIDS Output', slug: 'advanced-tutorials/bidsoutput' },
+						{ label: 'MultiQC', slug: 'advanced-tutorials/multiqc' },
+					],
+				},
+				{
+					label: 'Contribute to nf-neuro',
+					items: [
+						{ label: 'Create your module',
+							items: [
+								'contribute/create-your-module/template',
+								'contribute/create-your-module/main',
+								'contribute/create-your-module/documentation',
+								'contribute/create-your-module/tests',
+								'contribute/create-your-module/arguments',
+								'contribute/create-your-module/submit',
+								'contribute/create-your-module/infrastructure',
+								'contribute/create-your-module/resources',
+							]
+						},
+						{ label: 'Create your subworkflow',
+							items: [
+								'contribute/create-your-subworkflow/subworkflows',
+							]
+						}
+					]
+				},
+				{
+					label: 'API Documentation',
+					autogenerate: { directory: 'api' },
+					collapsed: true,
+				},
+			],
+		}),
+	],
 });
