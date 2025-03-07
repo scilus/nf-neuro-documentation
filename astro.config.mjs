be92e0ec1ae38ca3d25cc1878c58417d1f9e6142
Anthony Gagnon <anthony.gagnon7@usherbrooke.ca>
2025-03-07 15:09:26 -0500

    add versioning capabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightVersions from 'starlight-versions';
 
 // https://astro.build/config
 export default defineConfig({
@@ -71,6 +72,16 @@ export default defineConfig({
 					autogenerate: { directory: 'api' },
 					collapsed: true,
 				},
+			],
+			plugins: [
+				starlightVersions({
+					versions: [
+						{
+							slug: '0.0.1',
+							label: 'v0.0.1',
+						},
+					],
+				}),
 			],
 		}),
 	],
